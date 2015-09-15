0332b24ad50276dbb68869445a332befc300de6e
黄子毅 <576625322@qq.com>
2015-09-15 19:01:21 +0800

    add muk

--- build.sh
+++ build.sh
@@ -2,18 +2,18 @@
 
 if [[ $1 == "install" ]]
 then
-  echo 'install fis3..'
+  echo '[install fis3..]'
   npm install -g fis3
 
-  echo 'intall pm2..'
+  echo '[intall pm2..]'
   npm install -g pm2
 
-  echo 'install fis plugs..'
+  echo '[install fis plugs..]'
   npm install fis-parser-babelcore
   npm install fis-parser-less
   npm install fis-optimizer-html-minifier
 
-  echo 'install koa plugs'
+  echo '[install koa plugs]'
   npm install koa
   npm install koa-router
   npm install koa-redis
@@ -23,28 +23,36 @@ then
   npm install koa-etag
   npm install koa-bodyparser
 
-  echo 'install server plugs'
+  echo '[install server plugs]'
   npm install node-schedule
   npm install mysql
+  npm install redis
   npm install sequelize
   npm install validator
   npm install nodemailer
   npm install nodemailer-smtp-transport
 
-  echo 'install code cover plugs'
+  echo '[install code cover plugs]'
   npm install blanket
 
-  echo 'install test plugs'
+  echo '[install test plugs]'
   npm install -g mocha
   npm install should
+  npm install co-mocha
+  npm install muk
 fi
 
-echo 'run test'
-mocha --harmony --require blanket "test/**/*.js"
+echo '[run cover]'
+mocha --harmony -R travis-cov "test/**/*.js"
+echo '[created coverage.html]'
+mocha --harmony -R html-cov > coverage.html "test/**/*.js"
 
-echo 'run pm2'
+echo '[run test]'
+mocha --harmony "test/**/*.js"
+
+echo '[run pm2]'
 pm2 delete all
 pm2 start app.js --node-args="--harmony" --watch --ignore-watch="src static test node_modules" --merge-logs -f
 
-echo 'run fis3 dev mode..'
+echo '[run fis3 dev mode..]'
 fis3 release -d static -r src -f fis-conf.js -w -l
--- test/lib/sign.js
+++ test/lib/sign.js
@@ -1,4 +1,3 @@
-require("should")
 var sign = require('../../lib/sign')
 var url = require('url')
 
@@ -17,7 +16,7 @@ describe("sign", function () {
         // 获取签名url参数
         var urlParam = url.parse(signUrl, true).query
         var ok = sign.check(token, urlParam).ok
-        ok.should.eql(true)
+        ok.should.equal(true)
     })
 
     it("过期的签名无法通过", function () {
@@ -34,7 +33,43 @@ describe("sign", function () {
         // 获取签名url参数
         var urlParam = url.parse(signUrl, true).query
         var ok = sign.check(token, urlParam).ok
-        ok.should.eql(false)
+        ok.should.equal(false)
+    })
+
+    it("缺少签名无法通过", function () {
+        // 随机token
+        var token = 'vghj23b4oih3g32jk'
+
+        // 生成签名url
+        var signUrl = sign.create(token, -1, {
+            nickname: '小明',
+            password: '123',
+            email: '123456@qq.com'
+        })
+
+        // 获取签名url参数
+        var urlParam = url.parse(signUrl, true).query
+        urlParam.sign = ''
+        var ok = sign.check(token, urlParam).ok
+        ok.should.equal(false)
+    })
+
+    it("缺少过期时间无法通过", function () {
+        // 随机token
+        var token = 'vghj23b4oih3g32jk'
+
+        // 生成签名url
+        var signUrl = sign.create(token, -1, {
+            nickname: '小明',
+            password: '123',
+            email: '123456@qq.com'
+        })
+
+        // 获取签名url参数
+        var urlParam = url.parse(signUrl, true).query
+        urlParam.expire = ''
+        var ok = sign.check(token, urlParam).ok
+        ok.should.equal(false)
     })
 
     it("不正确签名无法通过", function () {
@@ -55,6 +90,6 @@ describe("sign", function () {
         urlParam.password = '456'
 
         var ok = sign.check(token, urlParam).ok
-        ok.should.eql(false)
+        ok.should.equal(false)
     })
 })
